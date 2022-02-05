60be4b552e80fb45a57556429bd9c9506a8215e6
TS <tserrante@gmail.com>
2022-02-05 14:38:16 -0500

    user can switch color via color palette

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // row x col grid
-const cols = 8;
-const rows = 8;
+const cols = 32;
+const rows = 32;
+
+
+
+// From color palette
+const colorPalette = document.querySelector('#palette');
+
+// sets color variable through event listener
+function setColor(e) {
+    color = e.target.value;
+}
+
+//Color to sketch with. Initially default value of input element
+// is set variably thorugh function paletteColor
+var color = colorPalette.getAttribute('value');
 
 // get grid container and give it a size of rows x cols
 const gridContainer = document.querySelector('.grid-container');
@@ -16,7 +30,13 @@ for (let i = 0; i < (rows * cols); i++) {
 
 const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach((item) => {
+
+
+
     item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'red';
+        item.style.backgroundColor = color;
     });
 });
+
+
+colorPalette.addEventListener('change', setColor);
