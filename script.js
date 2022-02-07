e54a1a5c9b869e563fa4c2dfc349552acd45a05c
TS <tserrante@gmail.com>
2022-02-07 12:53:36 -0500

    Added brush, background, rainbow button.  Added rainbow paint feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DEFAULT_SIZE = 8;
+const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#000000';
 const DEFAULT_MODE = 'color';
 const DEFAULT_BACKGROUND = '#ffffff';
@@ -19,7 +19,7 @@ function setCurrentMode(newMode) {
 
 function setCurrentSize(newSize) {
     // increment grid size in powers of 2
-    // from 2^3 -> 2^7
+    // from 2^4 -> 2^7
     let power = newSize;
     let result = Math.pow(2, power);
     // display above slider
@@ -33,20 +33,23 @@ function setCurrentBackground(newColor) {
 const display = document.querySelector("#slider-display");
 const slider = document.querySelector('#slider');
 const colorPalette = document.querySelector('#palette');
+const colorButton = document.querySelector('#palette-select');
 const backgroundPalette = document.querySelector('#background-palette');
+const backgroundButton = document.querySelector('#background-select');
+const rainbowColor = document.querySelector('#rainbow');
 const clearButton = document.querySelector('#clearBtn');
 const eraser = document.querySelector('#eraser');
 const gridContainer = document.querySelector('.grid-container');
 
 colorPalette.addEventListener('change', e => setCurrentColor(e.target.value));
+colorButton.addEventListener('click', () => setCurrentMode('color'));
 backgroundPalette.addEventListener('change', e => changeBackgroundColor(e.target.value));
+rainbowColor.addEventListener('click', () => setCurrentMode('rainbow'));
 clearButton.addEventListener('click', () => {
     clearGrid();
     setupGrid(currentSize);
 });
-eraser.addEventListener('click', () => {
-    setCurrentColor('white');
-});
+eraser.addEventListener('click', () => setCurrentMode('eraser'));
 slider.addEventListener('mousemove', (e) => updateDisplay(e.target.value));
 slider.addEventListener('change', (e) => changeSize(e.target.value));
 
@@ -57,9 +60,7 @@ function setupGrid() {
     // fill the grid container with grid items
     for (let i = 0; i < (currentSize * currentSize); i++) {
         const gridItem = document.createElement('div'); // create grid item
-        gridItem.addEventListener('mouseover', () => {
-            gridItem.style.backgroundColor = currentColor;
-        });
+        gridItem.addEventListener('mouseover', (e) => changeColor(e));
         gridItem.className = "grid-item";
         gridItem.style.setProperty('--background-color', currentBackground);
         gridContainer.appendChild(gridItem); // set style and add to grid
@@ -90,6 +91,29 @@ function changeBackgroundColor(color) {
 function updateDisplay(size) {
     display.innerText = `${0+currentSize} x ${0+currentSize}`
 }
+
+
+function changeColor(e) {
+    if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+
+    }
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    }
+    if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = currentBackground;
+    }
+}
+
+function calcRainbowColor() {
+
+    return rainbowValue;
+}
+
 window.addEventListener('load', () => {
     setupGrid();
     updateDisplay(currentSize);
